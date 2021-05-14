8be88bcd26b72a5d26ab10d000ef5c40751f6aab
Jason <429978+spaceyjase@users.noreply.github.com>
2021-05-14 15:16:02 +0100

    Minor tweaks.

--- Track/Camera.cs
+++ Track/Camera.cs
@@ -7,6 +7,7 @@ public class Camera : Godot.Camera
   [Export] private NodePath followThisPath;
   [Export] private float targetDistanceInMeters = 10f;
   [Export] private float targetHeightInMeters = 1f;
+  [Export] private Vector3 offset = new Vector3(0f, 2f, 0f);
 
   private Spatial followThis;
   private Vector3 lastLookAt;
@@ -17,11 +18,10 @@ public class Camera : Godot.Camera
 
     SetAsToplevel(true); // ignore parent transforms
 
-    if (followThisPath != null)
-    {
-      followThis = GetNode<Spatial>(followThisPath);
-      lastLookAt = followThis.GlobalTransform.origin;
-    }
+    if (followThisPath == null) return;
+    
+    followThis = GetNode<Spatial>(followThisPath);
+    lastLookAt = followThis.GlobalTransform.origin;
   }
 
   public override void _PhysicsProcess(float delta)
@@ -33,7 +33,7 @@ public class Camera : Godot.Camera
     var deltaV = GlobalTransform.origin - followThis.GlobalTransform.origin;
     var targetPosition = GlobalTransform.origin;
 
-    deltaV.y = 0; // no up and down movement
+    deltaV.y = 0f; // no up and down movement
     if (deltaV.Length() > targetDistanceInMeters)
     {
       deltaV = deltaV.Normalized() * targetDistanceInMeters;
@@ -48,6 +48,6 @@ public class Camera : Godot.Camera
     var newPosition = GlobalTransform.origin.LinearInterpolate(targetPosition, 1f);
     GlobalTransform = new Transform(Basis.Identity,  newPosition);
     lastLookAt = lastLookAt.LinearInterpolate(followThis.GlobalTransform.origin, 1f);
-    LookAt(lastLookAt + new Vector3(0f, 2f, 0f), Vector3.Up);
+    LookAt(lastLookAt + offset, Vector3.Up);
   }
 }
